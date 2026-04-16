7b073a91daf4983244c7396863f27de0d55aa181
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-04-16 17:37:02 +0700

    data 7

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -57,5 +57,13 @@ void display()
     cout << "  Elemen Array Setelah Diurutkan (Asc)\n";
     cout << "======================================\n";
 
-    
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
 }
